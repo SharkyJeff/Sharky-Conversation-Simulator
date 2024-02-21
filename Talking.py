09166155c913a29f3303776b958b75444fb4f741
Fionn Mahony Sorensen <96495849+SharkyJeff@users.noreply.github.com>
2024-02-21 16:51:01 +0000

    talking.py

--- Talking.py
+++ Talking.py
@@ -1,2 +1,14 @@
-print("Welcome to the Sharky Jeff Conversation Simulator, the advanced artificial intelligence model to fulfill all your Sharky Jeff needs.")
-input("Begin your conversation: ")
+## Talking.py
+## This is a program to accurately simulate conversations with Sharky Jeff.
+print("Welcome to the Sharky Jeff Conversation Simulator 1000, the advanced artificial intelligence model to fulfill all your Sharky Jeff needs.")
+for x in range(5):
+    input("Type here: ")
+    
+    import random
+    message = random.randint(1,5)
+    if message < 2:
+        print("ur cute")
+    else:
+        print("ur a fag")
+
+print("I'm going to go do homework now byebye")
